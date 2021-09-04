0431edd142d5afcd1f7ed2395f94c14ca82fb0c6
Nick Durand <nickfh7@gmail.com>
2021-09-03 21:20:39 -0700

    All cli commands have been implemented.

--- testing/colte_tests/main.py
+++ testing/colte_tests/main.py
@@ -10,16 +10,51 @@ def dev_main():
 
   ueransim = UeransimNode("ueransim", vagrant_dir=vagrant_dir)
   colte1 = ColteNode("colte1", vagrant_dir=vagrant_dir)
-  # colte2 = ColteNode("colte2", vagrant_dir=vagrant_dir)
 
   print("Hello world test:")
   print(" ueransim -", ueransim.run_command("echo hello world"))
   print(" colte1 -", colte1.run_command("echo hello world"))
-  # print(" colte2 -", colte2.run_command("echo hello world"))
   print()
   
   print("Device IDs test:")
   print(" ueransim -", ueransim.get_device_ids())
+  print()
+
+  print("Device build test")
+  ueransim.detect_devices()
+  print(" GNBs: ", ", ".join([d.id for d in ueransim.gnbs]))
+  print(" UEs: ", ", ".join([d.id for d in ueransim.ues]))
+  print()
+
+  print("Device info test")
+  print(" GNB info\n", ueransim.gnbs[0].info().replace("\n", "\n  "))
+  print(" UE info\n", ueransim.ues[0].info().replace("\n", "\n  "))
+  print()
+
+  print("Device status test")
+  print(" GNB status\n", ueransim.gnbs[0].status().replace("\n", "\n  "))
+  print(" UE status\n", ueransim.ues[0].status().replace("\n", "\n  "))
+  print()
+
+  print("GNB various commands test")
+  print(" amf_list\n", ueransim.gnbs[0].amf_list().replace("\n", "\n  "))
+  print(" amf_info\n", ueransim.gnbs[0].amf_info("0").replace("\n", "\n  "))
+  print(" amf_info\n", ueransim.gnbs[0].amf_info("2").replace("\n", "\n  "))
+  print(" ue_list\n", ueransim.gnbs[0].ue_list().replace("\n", "\n  "))
+  print(" ue_count\n", ueransim.gnbs[0].ue_count().replace("\n", "\n  "))
+  print(" ue_release\n", ueransim.gnbs[0].ue_release("0").replace("\n", "\n  "))
+  print()
+
+  print("UE various commands test")
+  print(" timers\n", ueransim.ues[0].timers().replace("\n", "\n  "))
+  print(" rls_state\n", ueransim.ues[0].rls_state().replace("\n", "\n  "))
+  print(" coverage\n", ueransim.ues[0].coverage().replace("\n", "\n  "))
+  print(" ps_establish\n", ueransim.ues[0].ps_establish("sc1 sc2").replace("\n", "\n  "))
+  print(" ps_list\n", ueransim.ues[0].ps_list().replace("\n", "\n  "))
+  print(" ps_release\n", ueransim.ues[0].ps_release("psid").replace("\n", "\n  "))
+  print(" ps_release_all\n", ueransim.ues[0].ps_release_all().replace("\n", "\n  "))
+  print(" deregister\n", ueransim.ues[0].deregister("sc").replace("\n", "\n  "))
+  print()
 
 
 if __name__ == "__main__":
--- testing/colte_tests/nodes/ueransim_node.py
+++ testing/colte_tests/nodes/ueransim_node.py
@@ -26,12 +26,28 @@ class UeransimNode(Node):
 
   def get_device_ids(self) -> List[str]:
     """
-    Queries all active UERANSIM devices.
+    Finds all active UERANSIM devices.
     Returns a list of all GNB and UE ids.
     """
     stdout = self.run_cli_command("-d")[0]
     
     return [id.strip() for id in stdout.split("\n") if id.strip() != '']
+
+  def detect_devices(self) -> None:
+    """
+    Finds active UERANSIM devices and creates objects representing them.
+    Replaces existing lists.
+    """
+    device_ids = self.get_device_ids()
+
+    self.gnbs.clear()
+    self.ues.clear()
+
+    for device_id in device_ids:
+      if device_id.startswith("imsi-"):
+        self.ues.append(UE(self, device_id))
+      else:
+        self.gnbs.append(GNB(self, device_id))
 
 
 class DeviceInstance:
@@ -49,13 +65,21 @@ class DeviceInstance:
     Runs a command on the device.
     Returns a tuple of stdout and stderr results.
     """
-    return self.node.run_cli_command(" ".join([self.id, "-e", command]))
+    return self.node.run_cli_command(" ".join([self.id, "-e", "\"{}\"".format(command)]))
 
-  def info(self) -> Union[str, str]:
+  def info(self) -> str:
     """
-    Runs info command and returns a tuple of stdout and stderr results.
+    Runs info command and returns the results.
+    Help: 'Show some information about the UE/gNB'
     """
-    return self.run_device_command("info")
+    return self.run_device_command("info")[0]
+
+  def status(self) -> str:
+    """
+    Runs status command and returns the results.
+    Help: 'Show some status information about the UE/gNB'
+    """
+    return self.run_device_command("status")[0]
 
 
 class GNB(DeviceInstance):
@@ -63,8 +87,99 @@ class GNB(DeviceInstance):
   Represents a gNodeB instance on the ueransim node.
   """
 
+  def amf_list(self) -> str:
+    """
+    Runs amf-list command and returns the results.
+    Help: 'List all AMFs associated with the gNB'
+    """
+    return self.run_device_command("amf-list")[0]
+
+  def amf_info(self, amf_id: str) -> str:
+    """
+    Runs amf-info command on the provided amf_id and returns the results.
+    Help: 'Show some status information about the given AMF'
+    """
+    return self.run_device_command(" ".join(["amf-info", amf_id]))[0]
+
+  def ue_list(self) -> str:
+    """
+    Runs ue-list command and returns the results.
+    Help: 'List all UEs associated with the gNB'
+    """
+    return self.run_device_command("ue-list")[0]
+
+  def ue_count(self) -> str:
+    """
+    Runs ue-count command and returns the results.
+    Help: 'Print the total number of UEs connected the this gNB'
+    """
+    return self.run_device_command("ue-count")[0]
+
+  def ue_release(self, ue_id: str) -> str:
+    """
+    Runs ue-release command on the provided ue_id and returns the results.
+    Help: 'Request a UE context release for the given UE'
+    """
+    return self.run_device_command(" ".join(["ue-release", ue_id]))[0]
+
 
 class UE(DeviceInstance):
   """
   Represents a UE instance on the ueransim node.
   """
+
+  def timers(self) -> str:
+    """
+    Runs timers command and returns the results.
+    Help: 'Dump current status of the timers in the UE'
+    """
+    return self.run_device_command("timers")[0]
+
+  def rls_state(self) -> str:
+    """
+    Runs rls-state command and returns the results.
+    Help: 'Show status information about RLS'
+    """
+    return self.run_device_command("rls-state")[0]
+
+  def coverage(self) -> str:
+    """
+    Runs coverage command and returns the results.
+    Help: 'Dump available cells and PLMNs in the coverage'
+    """
+    return self.run_device_command("coverage")[0]
+
+  def ps_establish(self, subcommand: str) -> str:
+    """
+    Runs ps-establish command with the provided subcommand and returns the results.
+    Help: 'Trigger a PDU session establishment procedure'
+    """
+    return self.run_device_command(" ".join(["ps-establish", subcommand]))[0]
+
+  def ps_list(self) -> str:
+    """
+    Runs ps-list command and returns the results.
+    Help: 'List all PDU sessions'
+    """
+    return self.run_device_command("ps-list")[0]
+
+  def ps_release(self, ps_id: str) -> str:
+    """
+    Runs ps-release command with the provided ps_id and returns the results.
+    Help: 'Trigger a PDU session release procedure'
+    """
+    return self.run_device_command(" ".join(["ps-release", ps_id]))[0]
+
+  def ps_release_all(self) -> str:
+    """
+    Runs ps-release-all command and returns the results.
+    Help: 'Trigger PDU session release procedures for all active sessions'
+    """
+    return self.run_device_command("ps-release-all")[0]
+
+  def deregister(self, subcommand: str) -> str:
+    """
+    Runs deregister command with the provided subcommand and returns the results.
+    Help: 'Perform a de-registration by the UE'
+    """
+    return self.run_device_command(" ".join(["deregister", subcommand]))[0]
