4c7a4346305a28fbdb58927622e47fc757fcd9a1
Matt Johnson <matt9j@cs.washington.edu>
2021-11-08 09:45:01 -0800

    manager-rs, protos: Incorporate local_authentication into manager
    In addition to adding the build and import code to the rust manager,
    this commit changes the package of the local_authentication proto to
    use snake_case instead of camelCase in the package name. Tonic and/or
    prost was unable to build and import the package for some reason with
    a camelCase name.

--- manager-rs/dauth-service/src/rpc/server.rs
+++ manager-rs/dauth-service/src/rpc/server.rs
@@ -3,6 +3,14 @@ use std::sync::Arc;
 use crate::data::context::DauthContext;
 use crate::rpc::handler::DauthHandler;
 
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod d_auth {
+    tonic::include_proto!("d_auth");
+}
+
+use d_auth::local_authentication_server::{LocalAuthentication, LocalAuthenticationServer};
+
 pub async fn start_server(context: Arc<DauthContext>) {
     println!("Starting server");
     // Testing until rpc/proto is ready
