6db0011b26c0d6fe1675d4b4c2f8a7f3ba675edc
Nick Durand <nickfh7@gmail.com>
2021-11-20 22:42:56 -0800

    Increment sqn tests.

--- manager-rs/dauth-service/src/data/user_info.rs
+++ manager-rs/dauth-service/src/data/user_info.rs
@@ -22,3 +22,34 @@ impl UserInfo {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::data::user_info::UserInfo;
+
+
+    #[test]
+    fn test_increment() {
+        assert!(check_increment_result(vec![0, 0, 0], vec![0, 0, 0], 0));
+        assert!(check_increment_result(vec![0, 0, 0], vec![0, 0, 1], 1));
+        assert!(check_increment_result(vec![0, 0, 0], vec![0, 0, 2], 2));
+        assert!(check_increment_result(vec![0, 0, 0], vec![0, 0, 255], 255));
+        assert!(check_increment_result(vec![0, 0, 0], vec![0, 1, 0], 256));
+        assert!(check_increment_result(vec![0, 0, 0], vec![1, 0, 0], 256*256));
+        assert!(check_increment_result(vec![0, 1, 0], vec![0, 1, 1], 1));
+        assert!(check_increment_result(vec![0, 1, 0], vec![0, 2, 0], 256));
+        assert!(check_increment_result(vec![0, 0, 0], vec![0, 0, 0], 256*256*256));
+    }
+
+    fn check_increment_result(v1: Vec<u8>, v2: Vec<u8>, amount: u64) -> bool {
+        let mut ui = UserInfo {
+            k: vec![],
+            opc: vec![],
+            sqn_max: v1,
+        };
+
+        ui.increment_sqn(amount);
+
+        ui.sqn_max == v2
+    }
+}
