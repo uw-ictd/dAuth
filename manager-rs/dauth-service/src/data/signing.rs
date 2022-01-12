68335a39b077a6304d58a77bcfb166ced0b42b61
Nick Durand <nickfh7@gmail.com>
2022-01-11 17:08:09 -0800

    Another partial commit with match statement.

--- manager-rs/dauth-service/src/data/signing.rs
+++ manager-rs/dauth-service/src/data/signing.rs
@@ -1,14 +1,48 @@
+use prost::Message;
+
 use crate::rpc::dauth::remote;
 
-/// All message types that expect an encrypted and signed payload
+/// All payload types that expect to be signed
 pub enum SignPayloadType {
-    DelegatedConfirmationShare(<remote::DelegatedConfirmationShare as Trait >::Payload),
-    GetHomeConfirmKeyReq(remote::GetHomeConfirmKeyReq::Payload),
-    EnrollBackupPrepareReq(remote::EnrollBackupPrepareReq::Payload),
-    EnrollBackupCommitReq(remote::EnrollBackupCommitReq::Payload),
-    GetBackupAuthVectorReq(remote::GetBackupAuthVectorReq::Payload),
-    GetKeyShareReq(remote::GetKeyShareReq::Payload),
-    WithdrawBackupReq(remote::WithdrawBackupReq::Payload),
-    WithdrawSharesReq(remote::WithdrawSharesReq::Payload),
-    FloodVectorReq(remote::FloodVectorReq::Payload),
+    GetHomeConfirmKeyReq(remote::get_home_confirm_key_req::Payload),
+    EnrollBackupPrepareReq(remote::enroll_backup_prepare_req::Payload),
+    GetBackupAuthVectorReq(remote::get_backup_auth_vector_req::Payload),
+    GetKeyShareReq(remote::get_key_share_req::Payload),
+    WithdrawBackupReq(remote::withdraw_backup_req::Payload),
+    WithdrawSharesReq(remote::withdraw_shares_req::Payload),
+    FloodVectorReq(remote::flood_vector_req::Payload),
+}
+
+pub fn sign_message(payload: SignPayloadType) -> remote::SignedMessage {
+    let (bytes, signed_type) = match payload {
+        SignPayloadType::GetHomeConfirmKeyReq(payload_message) => (
+            payload_message.encode_to_vec(),
+            remote::SignedMessageKind::GetHomeConfirmKeyReq,
+        ),
+        SignPayloadType::EnrollBackupPrepareReq(payload_message) => (
+            payload_message.encode_to_vec(),
+            remote::SignedMessageKind::EnrollBackupPrepareReq,
+        ),
+        SignPayloadType::GetBackupAuthVectorReq(payload_message) => (
+            payload_message.encode_to_vec(),
+            remote::SignedMessageKind::GetBackupAuthVectorReq,
+        ),
+        SignPayloadType::GetKeyShareReq(payload_message) => (
+            payload_message.encode_to_vec(),
+            remote::SignedMessageKind::GetKeyShareReq,
+        ),
+        SignPayloadType::WithdrawBackupReq(payload_message) => (
+            payload_message.encode_to_vec(),
+            remote::SignedMessageKind::WithdrawBackupReq,
+        ),
+        SignPayloadType::WithdrawSharesReq(payload_message) => (
+            payload_message.encode_to_vec(),
+            remote::SignedMessageKind::WithdrawSharesReq,
+        ),
+        SignPayloadType::FloodVectorReq(payload_message) => (
+            payload_message.encode_to_vec(),
+            remote::SignedMessageKind::FloodVectorReq,
+        ),
+    };
+    // TODO (nickfh7) Add signing logic, i.e. via dalek
 }
