cf4d4cb4ee77813f1e2e34d9640e230ce1580bd4
Matt Johnson <matt9j@cs.washington.edu>
2022-02-15 16:47:16 -0800

    [open5gs] Remove support components with security warnings
    The docs and webui are not needed for this project, and generate lots
    of spurious dependabot warnings due to their underlying technology
    ecosystem (web).

--- open5gs/docs/_pages/github.md
+++
@@ -1,5 +0,0 @@
----
-title: GitHub
-redirect_to:
-  - https://github.com/open5gs/open5gs
----
--- open5gs/webui/src/components/Profile/index.js
+++
@@ -1,9 +0,0 @@
-import List from './List';
-import Edit from './Edit';
-import View from './View';
-
-export {
-  List,
-  Edit,
-  View
-};
