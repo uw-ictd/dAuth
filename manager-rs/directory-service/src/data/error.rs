bacad182888d619fa83bf2c32850ed1d62d6a026
Nick Durand <nickfh7@gmail.com>
2022-04-22 13:46:33 -0700

    Add errors

---
+++ manager-rs/directory-service/src/data/error.rs
@@ -0,0 +1,23 @@
+use std::array::TryFromSliceError;
+
+use sqlx::Error as SqlxError;
+use thiserror::Error;
+
+/// General error type for directory service failures
+#[derive(Error, Debug)]
+pub enum DirectoryError {
+    #[error("Not found error -- {0}")]
+    NotFoundError(String),
+
+    #[error("Config error -- {0}")]
+    ConfigError(String),
+
+    #[error("Data error -- {0}")]
+    DataError(String),
+
+    #[error("Database error -- {0}")]
+    DatabaseError(#[from] SqlxError),
+
+    #[error("Conversion error -- {0}")]
+    ConversionError(#[from] TryFromSliceError),
+}
