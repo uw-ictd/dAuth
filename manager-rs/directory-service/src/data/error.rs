6469c17cf778ca2efafd4507468e9673e25d33d4
Nick Durand <nickfh7@gmail.com>
2022-04-23 01:13:13 -0700

    Refine errors

--- manager-rs/directory-service/src/data/error.rs
+++ manager-rs/directory-service/src/data/error.rs
@@ -6,20 +6,8 @@ use thiserror::Error;
 /// General error type for directory service failures
 #[derive(Error, Debug)]
 pub enum DirectoryError {
-    #[error("Not found error -- {0}")]
-    NotFoundError(String),
-
-    #[error("Config error -- {0}")]
-    ConfigError(String),
-
-    #[error("Data error -- {0}")]
-    DataError(String),
-
     #[error("Database error -- {0}")]
     DatabaseError(#[from] SqlxError),
-
-    #[error("Conversion error -- {0}")]
-    ConversionError(#[from] TryFromSliceError),
 
     #[error("Invalid access -- {0}")]
     InvalidAccess(String),
