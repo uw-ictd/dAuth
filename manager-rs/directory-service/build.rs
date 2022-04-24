f62ef87daf445ed87d7161ed5f39f50c408a0cc0
Nick Durand <nickfh7@gmail.com>
2022-04-23 19:29:58 -0700

    Remove unneeded client build

--- manager-rs/directory-service/build.rs
+++ manager-rs/directory-service/build.rs
@@ -1,6 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .build_client(true)
         .build_server(true)
         .compile(&["../../protos/directory.proto"], &["../../protos"])?;
     Ok(())
