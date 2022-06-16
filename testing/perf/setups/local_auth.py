91c08a5ecf8bf0a1f7695821cab395e3136ed0d3
Nick Durand <nickfh7@gmail.com>
2022-06-15 17:24:41 -0700

    Refactor run_perf for other setups

--- testing/perf/setups/local_auth.py
+++ testing/perf/setups/local_auth.py
@@ -1,13 +1,10 @@
 import os
-from logging import INFO
-from time import sleep
 
 from logger import TestingLogger
 
 from perf.config import ServiceConfig
 from perf.setups.common import NetworkSetup
 from perf.state import NetworkState
-from perf.metrics import PerfMetrics
 
 
 class LocalAuthSetup(NetworkSetup):
@@ -46,53 +43,4 @@ class LocalAuthSetup(NetworkSetup):
                 service_config.add_user(imsi, sqn_slice_max, backup_network_ids)
         
         self.state.service1.change_config(service_config)
-
-    def run_perf(self, num_ues: int, interval: int, iterations: int):
-        """
-        Configures the network for the provided setup.
-        Runs and prints the resulting performance metrics.
-        """
-        TestingLogger.logger.info("Running local auth perf")
-        TestingLogger.logger.info(
-            "Num UEs: {}, Inteval: {}ms, iterations: {}"
-            .format(num_ues, interval, iterations))
-        
-        try:
-            # configure and reset the network state 
-            self._configure(num_ues)
-            self.state.reset()
-            
-            # wait for network to settle
-            TestingLogger.logger.info("Waiting for network to settle")
-            sleep(5)
-            
-            # Start gnb and ues
-            TestingLogger.logger.info("Starting gNB and UEs")
-            self._start_gnb()
-            output, err = self._start_ues(num_ues, interval, iterations)
-            
-            TestingLogger.logger.info("Processing output (varies by iterations*interval)")
-            metrics = PerfMetrics()
-            for line in output:
-                try:
-                    metrics.add_result_from_json(line)
-                except Exception as e:
-                    TestingLogger.logger.debug("Failed<{}>: {}".format(e, line.rstrip()))
-                
-            for line in err:
-                TestingLogger.logger.debug("Stderr:", line.rstrip())
-                
-            print("Results:")
-            for name in metrics.get_names():
-                print(" ", name)
-                print("   cmd tags:", metrics.get_command_tags(name))
-                print("   values  :", metrics.get_results(name))
-                print("   averages:", metrics.get_average(name))
-            print(" ", "All")
-            print("   averages:", metrics.get_total_average())
-
-        except Exception as e:
-            TestingLogger.logger.error("Failed to run: {}".format(e))
-        
-        TestingLogger.logger.info("Perf completed")
 
