d3b57cc72cb6b6be483a9b434b80e7e899f913dc
Nick Durand <nickfh7@gmail.com>
2022-06-24 14:02:25 -0700

    Add metric set 2

---
+++ testing/perf/dauth_testing/metric_set_2/build_configs.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import yaml
+import copy
+import random
+from typing import List, Union
+
+def build_configs(config_dir: str, output_dir: str):
+    for config_name in os.listdir(config_dir):
+        config_content = yaml.safe_load(open(os.path.join(config_dir, config_name), 'r'))
+        other_configs = generate_variations(config_name, config_content)
+        
+        for (filename, content) in other_configs:
+            filepath = os.path.join(output_dir, filename)
+            with open(filepath, 'w') as of:
+                of.write(yaml.dump(content))
+        
+        
+def generate_variations(config_name: str, config_content: dict) -> List[Union[str,dict]]:
+    # set the home network (offline) to the ueransim node
+    config_content["services"][0] = copy.deepcopy(config_content["ueransim"])
+    config_content["services"][0]["id"] = \
+        config_content["services"][0]["id"].replace("-ueransim", "-service")
+    
+    other_configs = list()
+    
+    for num_backups in (2, 4, 8):
+        for random_set in range(10):
+            new_config = copy.deepcopy(config_content)
+            
+            backups = random.sample(new_config["services"][2:], num_backups)
+            new_config["services"] = new_config["services"][0:2]
+            new_config["services"].extend(backups)
+        
+            other_configs.append(
+                (config_name.replace(".yaml", "") + 
+                "-nbu{}-rs{}.yaml".format(num_backups, random_set),
+                new_config))
+        
+    return other_configs
+ 
+if __name__ == "__main__":
+    build_configs(sys.argv[1], sys.argv[2])
---
+++ testing/perf/dauth_testing/metric_set_2/run_tests.sh
@@ -0,0 +1,53 @@
+mkdir ./testing/perf/dauth_testing/metric_set_2/configs/ 2>/dev/null
+mkdir ./testing/perf/dauth_testing/metric_set_2/results/ 2>/dev/null
+
+rm ./testing/perf/dauth_testing/metric_set_2/configs/* 2>/dev/null
+rm ./testing/perf/dauth_testing/metric_set_2/results/* 2>/dev/null
+
+python3 ./testing/perf/dauth_testing/metric_set_2/build_configs.py \
+        ./testing/perf/dauth_testing/base/ \
+        ./testing/perf/dauth_testing/metric_set_2/configs/
+
+for FILE in ./testing/perf/dauth_testing/metric_set_2/configs/*
+do
+  NAME="$(basename $FILE .yaml)"
+
+  for THRESHOLD in 2 4 8
+  do
+    for NUM_UES in 1 5 10 20 50
+    do
+      case $THRESHOLD in
+        2)
+          # Always allow
+          ;;
+
+        4)
+          # Don't allow with only 2 backups
+          if [[ "$FILE" == *"nbu2"* ]]; then
+            break
+          fi
+          ;;
+
+        8)
+          # Don't allow unless 8 backups
+          if [[ ! "$FILE" == *"nbu8"* ]]; then
+            break
+          fi
+          ;;
+      esac
+
+      echo "Running $NAME with $NUM_UES UE(s) and $THRESHOLD threshold"
+      python3 ./testing/perf/run_perf.py \
+        -p "$FILE" \
+        -c ./testing/configs \
+        -u ./testing/perf/ue_driver.py \
+        -n "$NUM_UES" \
+        -i 2000 \
+        -t 10 \
+        -k "$THRESHOLD" \
+        --home-auth \
+        --debug \
+        >> ./testing/perf/dauth_testing/metric_set_2/results/"$NAME".out
+    done
+  done
+done
