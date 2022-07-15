1309a397cbe389ddac448a700dee0edada97a11a
Matt Johnson <matt9j@cs.washington.edu>
2022-07-14 20:52:40 -0700

    services/dauth: Create combined share type
    This implementation has a lot of copying, and could probably be
    refactored nicely with a large size reduction once the commonalities
    between how the different keys are used is better understood.

--- services/dauth-service/src/data/keys.rs
+++ services/dauth-service/src/data/keys.rs
@@ -1,12 +1,105 @@
 use tracing::instrument;
 
-use auth_vector::constants::KSEAF_LENGTH;
-use auth_vector::types::Kseaf;
+use auth_vector::types::{KSEAF_LENGTH, KASME_LENGTH};
+use auth_vector::types::{Kseaf,Kasme,XResStarHash,XResHash};
 
 use crate::data::error::DauthError;
 
 pub const TEMPORARY_CONSTANT_THRESHOLD: u8 = 3;
 const SHARE_LENGTH: usize = KSEAF_LENGTH + 1;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub struct CombinedKeyShare {
+    pub kseaf_share: KseafShare,
+    pub kasme_share: KasmeShare,
+    pub xres_star_hash: XResStarHash,
+    pub xres_hash: XResHash,
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub struct KasmeShare {
+    pub share: [u8; KASME_LENGTH + 1],
+}
+
+impl KasmeShare {
+    pub fn as_slice<'a>(&'a self) -> &'a [u8] {
+        self.share.as_slice()
+    }
+}
+
+impl TryFrom<Vec<u8>> for KasmeShare {
+    type Error = DauthError;
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        if value.len() != SHARE_LENGTH {
+            return Err(DauthError::ShamirShareError());
+        }
+
+        let res: Result<[u8; SHARE_LENGTH], _> = value.try_into();
+
+        res.and_then(|share_bytes| Ok(KasmeShare { share: share_bytes }))
+            .or(Err(DauthError::ShamirShareError()))
+    }
+}
+
+impl TryFrom<&[u8]> for KasmeShare {
+    type Error = DauthError;
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        if value.len() != SHARE_LENGTH {
+            return Err(DauthError::ShamirShareError());
+        }
+
+        let res: Result<[u8; SHARE_LENGTH], _> = value.try_into();
+
+        res.and_then(|share_bytes| Ok(KasmeShare { share: share_bytes }))
+            .or(Err(DauthError::ShamirShareError()))
+    }
+}
+
+#[instrument(level = "info")]
+pub fn create_shares_from_kasme<T: rand_0_8::RngCore + std::fmt::Debug>(
+    input: &Kasme,
+    share_count: u8,
+    threshold_share_count: u8,
+    rng: &mut T,
+) -> Result<Vec<KasmeShare>, DauthError> {
+    tracing::info!("Thingin the do");
+    if threshold_share_count > share_count {
+        tracing::error!(
+            share_count,
+            threshold_share_count,
+            "Invalid share request with threshold greater than total share count."
+        );
+        return Err(DauthError::ShamirShareError());
+    }
+    let secret = shamir::SecretData::with_secret_bytes(&input.as_array(), threshold_share_count, rng);
+
+    let mut shares: Vec<KasmeShare> = Vec::new();
+    for i in 1u8..share_count + 1 {
+        let share = secret.get_share(i).or_else(|e| {
+            tracing::error!(share_index = i, ?e, "Failed to get share");
+            Err(DauthError::ShamirShareError())
+        })?;
+        shares.push(share.try_into()?);
+    }
+
+    Ok(shares)
+}
+
+#[instrument(level = "debug")]
+pub fn recover_kasme_from_shares(
+    input: &Vec<KasmeShare>,
+    threshold: u8,
+) -> Result<Kasme, DauthError> {
+    let byte_inputs: Vec<Vec<u8>> = input.iter().map(|x| x.share.to_vec()).collect();
+
+    let res = shamir::SecretData::recover_secret_bytes(threshold, byte_inputs);
+
+    match res {
+        Some(value) => Ok(value.try_into().unwrap()),
+        None => Err(DauthError::ShamirShareError()),
+    }
+}
+
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct KseafShare {
@@ -202,6 +295,55 @@ mod tests {
                 ],
             },
             KseafShare {
+                share: [
+                    5, 83, 39, 126, 121, 172, 27, 144, 111, 113, 220, 234, 135, 113, 219, 48, 223,
+                    3, 119, 46, 41, 252, 75, 192, 63, 33, 140, 186, 215, 33, 139, 96, 47,
+                ],
+            },
+        ];
+
+        for (i, share) in res.iter().enumerate() {
+            assert_eq!(share, &expected_res[i]);
+        }
+    }
+
+    #[test]
+    fn test_vector_creation_deterministic_kasme() {
+        let mut rng = rand_0_8::rngs::mock::StepRng::new(1, 1);
+        let kasme: Kasme =
+            hex::decode("562d716dbd058b475cfecdbb48ed038f562d716dbd058b475cfecdbb48ed038f")
+                .unwrap()
+                .try_into()
+                .unwrap();
+        let res = create_shares_from_kasme(&kasme, 5, 3, &mut rng).unwrap();
+        println!("{:?}", res);
+
+        let expected_res = vec![
+            KasmeShare {
+                share: [
+                    1, 87, 47, 114, 105, 184, 3, 140, 79, 85, 244, 198, 183, 69, 227, 12, 159, 71,
+                    63, 98, 121, 168, 19, 156, 95, 69, 228, 214, 167, 85, 243, 28, 175,
+                ],
+            },
+            KasmeShare {
+                share: [
+                    2, 84, 41, 119, 101, 183, 9, 133, 87, 78, 234, 219, 163, 82, 241, 29, 175, 116,
+                    9, 87, 69, 151, 41, 165, 119, 110, 202, 251, 131, 114, 209, 61, 207,
+                ],
+            },
+            KasmeShare {
+                share: [
+                    3, 85, 43, 116, 97, 178, 15, 130, 95, 71, 224, 208, 175, 95, 255, 18, 191, 101,
+                    27, 68, 81, 130, 63, 178, 111, 119, 208, 224, 159, 111, 207, 34, 239,
+                ],
+            },
+            KasmeShare {
+                share: [
+                    4, 82, 37, 125, 125, 169, 29, 151, 103, 120, 214, 225, 139, 124, 213, 63, 207,
+                    18, 101, 61, 61, 233, 93, 215, 39, 56, 150, 161, 203, 60, 149, 127, 15,
+                ],
+            },
+            KasmeShare {
                 share: [
                     5, 83, 39, 126, 121, 172, 27, 144, 111, 113, 220, 234, 135, 113, 219, 48, 223,
                     3, 119, 46, 41, 252, 75, 192, 63, 33, 140, 186, 215, 33, 139, 96, 47,
