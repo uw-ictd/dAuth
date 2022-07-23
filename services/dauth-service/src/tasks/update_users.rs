03412bffa68312c7f40776dd7263e425ec30d8bf
Matt Johnson <matt9j@cs.washington.edu>
2022-07-23 00:22:14 -0700

    services/dauth: Flatten backup update transactions per user
    Transactions were being created within the inner loop of per each
    backup per each vector per each user... this makes the initial user
    update process very slow on nodes with slow disk. This commit
    refactors this code to move the transaction to the outermost loop, but
    at the expense of larger transaction chunks and bigger sections that
    would have to be recovered in the event of a failure.

--- services/dauth-service/src/tasks/update_users.rs
+++ services/dauth-service/src/tasks/update_users.rs
@@ -4,6 +4,7 @@ use std::io::prelude::*;
 use std::sync::Arc;
 
 use auth_vector::types::Rand;
+use auth_vector::data::AuthVectorData;
 
 use crate::core;
 use crate::data::context::DauthContext;
@@ -52,6 +53,12 @@ pub async fn run_task(context: Arc<DauthContext>) -> Result<(), DauthError> {
     Ok(())
 }
 
+struct CreatedVector {
+    pub backup_network_id: String,
+    pub seqnum: i64,
+    pub vector: AuthVectorData,
+}
+
 /// Adds the user and its backup networks to the directory service.
 /// Then, enrolls each of the backup networks.
 async fn handle_user_update(context: Arc<DauthContext>, user_id: String) -> Result<(), DauthError> {
@@ -85,118 +92,134 @@ async fn handle_user_update(context: Arc<DauthContext>, user_id: String) -> Resu
 
     directory::upsert_user(context.clone(), &user_id, backup_network_ids.clone()).await?;
 
-    /* create vectors and shares */
-    for (backup_network_id, sqn_slice) in &user_data {
-        // T2: Collect existing vectors (one transaction for each backup network)
+    let mut update_tasks: Vec<CreatedVector> = Vec::new();
+
+    {
+        // T2: Collect existing vectors (one transaction for all backup networks)
         let mut transaction = context.local_context.database_pool.begin().await.unwrap();
-        let num_existing_vectors =
+
+        // Loop over all backup networks and create all vectors required for
+        // each netowrk as part of one big database transaction together. This
+        // creates a task for each created auth vector to then create its key
+        // shares outside the transaction loop.
+        for (backup_network_id, sqn_slice) in &user_data {
+            let num_existing_vectors =
             database::vector_state::get_all_by_id(&mut transaction, user_id, backup_network_id)
                 .await?
                 .len() as i64;
-        transaction.commit().await?; // T2 end
 
-        if num_existing_vectors > 0 {
+            if num_existing_vectors > 0 {
+                tracing::info!(
+                    "Found {} existing vector(s) for {} on {}",
+                    num_existing_vectors,
+                    user_id,
+                    backup_network_id
+                );
+            }
+
             tracing::info!(
-                "Found {} existing vector(s) for {} on {}",
-                num_existing_vectors,
-                user_id,
-                backup_network_id
+                ?backup_network_id,
+                ?user_id,
+                "Building auth vectors for backup network"
             );
+
+            // for each vector (up to max), build a new vector and set of key shares
+            for _ in 0..std::cmp::max(
+                0,
+                context.local_context.max_backup_vectors - num_existing_vectors,
+            ) {
+                let (vector, seqnum) = core::auth_vectors::build_auth_vector(
+                    context.clone(),
+                    &mut transaction,
+                    user_id,
+                    *sqn_slice,
+                )
+                .await?;
+
+                update_tasks.push(CreatedVector { backup_network_id: backup_network_id.clone(), seqnum: seqnum, vector: vector });
+            }
         }
 
-        tracing::info!(
-            ?backup_network_id,
-            ?user_id,
-            "Building auth vectors for backup network"
+        transaction.commit().await?; // T2 end
+    }
+
+    /* create vectors and shares */
+    for task in update_tasks
+    {
+        let vector = task.vector;
+        let backup_network_id = &task.backup_network_id;
+        let seqnum = task.seqnum;
+
+        let (xres_star_hash, xres_hash, rand) = (
+            vector.xres_star_hash.clone(),
+            vector.xres_hash.clone(),
+            vector.rand.clone(),
         );
+        let mut rng = rand_0_8::thread_rng();
 
-        // for each vector (up to max), build a new vector and set of key shares
-        for _ in 0..std::cmp::max(
-            0,
-            context.local_context.max_backup_vectors - num_existing_vectors,
-        ) {
-            // T3: Build auth vector (up to max backups for each backup network)
-            let mut transaction = context.local_context.database_pool.begin().await.unwrap();
-            let (vector, seqnum) = core::auth_vectors::build_auth_vector(
-                context.clone(),
-                &mut transaction,
-                user_id,
-                *sqn_slice,
-            )
-            .await?;
-            transaction.commit().await?; // T3 end
-
-            let (xres_star_hash, xres_hash, rand) = (
-                vector.xres_star_hash.clone(),
-                vector.xres_hash.clone(),
-                vector.rand.clone(),
-            );
-            let mut rng = rand_0_8::thread_rng();
-
-            let kseaf_shares = keys::create_shares_from_kseaf(
-                &vector.kseaf,
+        let kseaf_shares = keys::create_shares_from_kseaf(
+            &vector.kseaf,
+            backup_network_ids.len() as u8,
+            std::cmp::min(
+                context.backup_context.backup_key_threshold,
                 backup_network_ids.len() as u8,
-                std::cmp::min(
-                    context.backup_context.backup_key_threshold,
-                    backup_network_ids.len() as u8,
-                ),
-                &mut rng,
-            )?;
+            ),
+            &mut rng,
+        )?;
 
-            let kasme_shares = keys::create_shares_from_kasme(
-                &vector.kasme,
+        let kasme_shares = keys::create_shares_from_kasme(
+            &vector.kasme,
+            backup_network_ids.len() as u8,
+            std::cmp::min(
+                context.backup_context.backup_key_threshold,
                 backup_network_ids.len() as u8,
-                std::cmp::min(
-                    context.backup_context.backup_key_threshold,
-                    backup_network_ids.len() as u8,
-                ),
-                &mut rng,
-            )?;
+            ),
+            &mut rng,
+        )?;
 
-            let mut shares: Vec<(keys::CombinedKeyShare, Rand)> = Vec::new();
+        let mut shares: Vec<(keys::CombinedKeyShare, Rand)> = Vec::new();
 
-            for (kseaf_share, kasme_share) in std::iter::zip(kseaf_shares, kasme_shares) {
-                shares.push((
-                    keys::CombinedKeyShare {
-                        xres_star_hash: xres_star_hash,
-                        xres_hash: xres_hash,
-                        kasme_share: kasme_share,
-                        kseaf_share: kseaf_share,
-                    }
-                    .to_owned(),
-                    rand,
-                ));
-            }
+        for (kseaf_share, kasme_share) in std::iter::zip(kseaf_shares, kasme_shares) {
+            shares.push((
+                keys::CombinedKeyShare {
+                    xres_star_hash: xres_star_hash,
+                    xres_hash: xres_hash,
+                    kasme_share: kasme_share,
+                    kseaf_share: kseaf_share,
+                }
+                .to_owned(),
+                rand,
+            ));
+        }
 
-            vectors_map
-                .get_mut(backup_network_id)
-                .ok_or(DauthError::DataError("Vectors map error".to_string()))?
-                .push(AuthVectorRes {
-                    user_id: user_id.to_string(),
-                    seqnum,
-                    rand: vector.rand,
-                    autn: vector.autn,
-                    xres_star_hash: vector.xres_star_hash,
-                    xres_hash: vector.xres_hash,
-                });
+        vectors_map
+            .get_mut(backup_network_id)
+            .ok_or(DauthError::DataError("Vectors map error".to_string()))?
+            .push(AuthVectorRes {
+                user_id: user_id.to_string(),
+                seqnum,
+                rand: vector.rand,
+                autn: vector.autn,
+                xres_star_hash: vector.xres_star_hash,
+                xres_hash: vector.xres_hash,
+            });
 
-            for other_id in &backup_network_ids {
-                shares_map
-                    .get_mut(other_id)
-                    .ok_or(DauthError::DataError("Shares map error".to_string()))?
-                    .push(shares.pop().ok_or(DauthError::DataError(
-                        "Shares list out of shares".to_string(),
-                    ))?);
-            }
+        for other_id in &backup_network_ids {
+            shares_map
+                .get_mut(other_id)
+                .ok_or(DauthError::DataError("Shares map error".to_string()))?
+                .push(shares.pop().ok_or(DauthError::DataError(
+                    "Shares list out of shares".to_string(),
+                ))?);
+        }
 
-            if !shares.is_empty() {
-                tracing::warn!("{} unused share(s) after share generation", shares.len())
-            }
+        if !shares.is_empty() {
+            tracing::warn!("{} unused share(s) after share generation", shares.len())
         }
     }
 
     /* enroll backups */
-    for (backup_network_id, seqnum_slice) in &user_data {
+    for (backup_network_id, _) in &user_data {
         let (address, _) = directory::lookup_network(&context, &backup_network_id).await.or_else(
             |e| {
                 tracing::error!(?e, "Failed to enroll backup");
@@ -223,37 +246,6 @@ async fn handle_user_update(context: Arc<DauthContext>, user_id: String) -> Resu
                 Err(e)
             })?;
 
-        // T4: Enroll backup network, then store vector states
-        let mut transaction = context.local_context.database_pool.begin().await.unwrap();
-        database::backup_networks::upsert(
-            &mut transaction,
-            user_id,
-            backup_network_id,
-            *seqnum_slice,
-        )
-        .await?;
-        for vector in vectors {
-            database::vector_state::add(
-                &mut transaction,
-                &vector.xres_star_hash,
-                user_id,
-                backup_network_id,
-            )
-            .await?;
-        }
-        for (combined_share, rand) in shares {
-            database::key_share_state::add(
-                &mut transaction,
-                &combined_share.xres_star_hash,
-                &combined_share.xres_hash,
-                backup_network_id,
-                user_id,
-                &rand.as_array(),
-            )
-            .await?;
-        }
-        transaction.commit().await?; // T4 end
-
         // drop rand before sending
         // TODO: allow backups to have rand? Would allow them to check res
         let key_shares: Vec<keys::CombinedKeyShare> = shares
@@ -277,6 +269,48 @@ async fn handle_user_update(context: Arc<DauthContext>, user_id: String) -> Resu
                 tracing::error!(?e, ?user_id, ?backup_network_id, "Failed to enroll backup commit");
                 Err(e)
             })?;
+    }
+
+    {
+        let mut transaction = context.local_context.database_pool.begin().await.unwrap();
+        for (backup_network_id, seqnum_slice) in &user_data {
+            let vectors = vectors_map
+            .get(backup_network_id)
+            .ok_or(DauthError::DataError("Failed to get vectors".to_string()))?;
+            let shares = shares_map
+                .get(backup_network_id)
+                .ok_or(DauthError::DataError("Failed to get shares".to_string()))?;
+
+            // T4: Enroll backup network, then store vector states
+            database::backup_networks::upsert(
+                &mut transaction,
+                user_id,
+                backup_network_id,
+                *seqnum_slice,
+            )
+            .await?;
+            for vector in vectors {
+                database::vector_state::add(
+                    &mut transaction,
+                    &vector.xres_star_hash,
+                    user_id,
+                    backup_network_id,
+                )
+                .await?;
+            }
+            for (combined_share, rand) in shares {
+                database::key_share_state::add(
+                    &mut transaction,
+                    &combined_share.xres_star_hash,
+                    &combined_share.xres_hash,
+                    backup_network_id,
+                    user_id,
+                    &rand.as_array(),
+                )
+                .await?;
+            }
+        }
+        transaction.commit().await?; // T4 end
     }
 
     // T5: Task is complete, so remove
