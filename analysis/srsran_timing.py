4231f463502892f8b0eb65abcfa047a13653895c
Matt Johnson <matt9j@cs.washington.edu>
2022-08-13 12:42:28 +0200

    analysis: Make srsran plots

--- analysis/srsran_timing.py
+++ analysis/srsran_timing.py
@@ -25,19 +25,21 @@ def canonicalize_all_logs(input_dir, intermediate_file):
     """
     # Read in and build parquet from raw data
     dataframes = [
-        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-open5gs-nova233-cbrs-20MHz-unloaded.log", -1, "open5gs"),
-        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-nova233-cbrs-20MHz-unloaded.log", 8, "dauth"),
-        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-nova233-cbrs-20MHz-unloaded-accidental-dupe.log", 8, "dauth"),
-        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-bt-6-nova233-cbrs-20MHz-unloaded.log", 6, "dauth"),
-        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-bt-4-nova233-cbrs-20MHz-unloaded.log", 4, "dauth"),
-        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-bt-2-nova233-cbrs-20MHz-unloaded.log", 2, "dauth")
+        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-open5gs-nova233-cbrs-20MHz-unloaded.log", -1, "open5gs", "unloaded"),
+        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-open5gs-nova233-cbrs-20MHz-loaded-iperf.log", -1, "open5gs", "loaded"),
+
+        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-nova233-cbrs-20MHz-unloaded.log", -1, "dauth-home-online", "unloaded"),
+        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-nova233-cbrs-20MHz-unloaded-accidental-dupe.log", -1, "dauth-home-online", "unloaded"),
+        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-bt-6-nova233-cbrs-20MHz-unloaded.log", 6, "dauth-backup", "unloaded"),
+        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-bt-4-nova233-cbrs-20MHz-unloaded.log", 4, "dauth-backup", "unloaded"),
+        normalize_log_to_dataframe(input_dir/"srslte-ue-attach-loop-dauth-s-h-cobble-dauth-hestia-bt-2-nova233-cbrs-20MHz-unloaded.log", 2, "dauth-backup", "unloaded"),
     ]
     df = pd.concat(dataframes, axis=0)
     intermediate_file.parent.mkdir(exist_ok=True)
     df.to_parquet(intermediate_file)
     log.info("Successfully parsed %d rows from all input files", len(df))
 
-def normalize_log_to_dataframe(filename, threshold, condition):
+def normalize_log_to_dataframe(filename, threshold, condition, load):
     """ Parses and processes a single log into a dataframe for further analysis
     """
     datapoints = []
@@ -59,11 +61,119 @@ def normalize_log_to_dataframe(filename, threshold, condition):
                 "attach_time_ms": float(attach_time_ns)/1_000_000.0,
                 "threshold": threshold,
                 "condition": condition,
+                "load": load,
                 })
 
     log.info("[%s] Successfully parsed %d rows", filename, len(datapoints))
-    return pd.DataFrame(data=datapoints)
+    df = pd.DataFrame(data=datapoints)
 
+    def label_maker(row):
+        out = row["condition"] + "-" + row["load"]
+        if row["threshold"] > 0:
+            out = out + "-threshold[" +str(row["threshold"]) + "]"
+        return out
+
+    df["label"] = df.apply(label_maker, axis=1)
+
+    return df
+
+def make_boxplots(df: pd.DataFrame, chart_output_path: Path):
+    chart_output_path.mkdir(parents=True, exist_ok=True)
+    print(len(df), df.head())
+
+    alt.Chart(df).mark_boxplot().encode(
+        x=alt.X(
+            "label:O"
+        ),
+        y=alt.Y(
+            "attach_time_ms:Q",
+            title="Attach Time (ms)",
+            axis=alt.Axis(labels=True),
+            # scale=alt.Scale(
+            #     type="symlog"
+            # ),
+        ),
+        color=alt.Color(
+            "condition:N",
+            scale=alt.Scale(scheme="tableau20"),
+        ),
+    ).properties(
+        width=500,
+    ).save(
+        chart_output_path/"srsran-complete-boxplot.png",
+        scale_factor=2,
+    )
+
+    df = df.loc[df["attach_time_ms"] < 1000.0]
+
+    alt.Chart(df).mark_boxplot().encode(
+        x=alt.X(
+            "label:O"
+        ),
+        y=alt.Y(
+            "attach_time_ms:Q",
+            title="Attach Time (ms)",
+            axis=alt.Axis(labels=True),
+            # scale=alt.Scale(
+            #     type="symlog"
+            # ),
+        ),
+        color=alt.Color(
+            "condition:N",
+            scale=alt.Scale(scheme="tableau20"),
+        ),
+    ).properties(
+        width=500,
+    ).save(
+        chart_output_path/"srsran-boxplot-drop-outliers.png",
+        scale_factor=2,
+    )
+
+def generate_cdf_series(df, filter_column, filter_value, value_column):
+    stats_frame = df.reset_index()
+    stats_frame = stats_frame.loc[stats_frame[filter_column] == filter_value]
+    stats_frame = stats_frame.groupby([value_column]).count()[["index"]].rename(columns = {"index": "sample_count"})
+    stats_frame["pdf"] = stats_frame["sample_count"] / sum(stats_frame["sample_count"])
+    stats_frame["cdf"] = stats_frame["pdf"].cumsum()
+    stats_frame[filter_column] = filter_value
+    stats_frame = stats_frame.reset_index()
+    return stats_frame
+
+def make_cdfs(df: pd.DataFrame, chart_output_path: Path):
+    chart_output_path.mkdir(parents=True, exist_ok=True)
+
+    # Compute a cdf over observed latencies separately for each scenario
+    plot_frame = None
+    for scenario in df["label"].unique():
+        if plot_frame is None:
+            plot_frame = generate_cdf_series(df, "label", scenario, "attach_time_ms")
+        else:
+            plot_frame = pd.concat([plot_frame, generate_cdf_series(df, "label", scenario, "attach_time_ms")], ignore_index=True)
+
+    df = plot_frame.reset_index()
+
+    alt.Chart(df).mark_line().encode(
+        x=alt.X(
+            "attach_time_ms:Q"
+        ),
+        y=alt.Y(
+            "cdf:Q",
+            title="Attach Time (ms)",
+            axis=alt.Axis(labels=True),
+            # scale=alt.Scale(
+            #     type="symlog"
+            # ),
+        ),
+        color=alt.Color(
+            "label:N",
+            scale=alt.Scale(scheme="tableau20"),
+        ),
+    ).properties(
+        width=500,
+    ).save(
+        chart_output_path/"srsran-complete-cdfs.png",
+        scale_factor=2,
+    )
 
 if __name__ == "__main__":
     raw_logs_path = Path("data/srsran/raw")
@@ -77,4 +187,5 @@ if __name__ == "__main__":
     chart_renders_dir = Path("scratch/renders")
     df = pd.read_parquet(intermediate_file)
     print(df.head())
-    # make_plot(df, charts_path)
+    # make_boxplots(df, chart_renders_dir)
+    make_cdfs(df, chart_renders_dir)
