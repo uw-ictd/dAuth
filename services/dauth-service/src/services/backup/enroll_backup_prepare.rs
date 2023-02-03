27723d478899ef29de07e99c686811d177f8129e
Nick Durand <nickfh7@gmail.com>
2023-02-03 10:45:57 -0800

    backup::enroll_backup_prepare clean up and logging

--- services/dauth-service/src/services/backup/enroll_backup_prepare.rs
+++ services/dauth-service/src/services/backup/enroll_backup_prepare.rs
@@ -4,15 +4,17 @@ use crate::data::{context::DauthContext, error::DauthError};
 use crate::database;
 
 /// Sets the provided user id as a being backed up by this network.
+#[tracing::instrument(skip(context), name = "backup::enroll_backup_prepare")]
 pub async fn enroll_backup_prepare(
     context: Arc<DauthContext>,
     user_id: &str,
     home_network_id: &str,
 ) -> Result<(), DauthError> {
-    tracing::info!("Setting backup user: {:?} - {:?}", user_id, home_network_id);
+    tracing::info!("Preparing backup enrollment");
 
     let mut transaction = context.local_context.database_pool.begin().await?;
     database::backup_users::add(&mut transaction, user_id, home_network_id).await?;
     transaction.commit().await?;
+
     Ok(())
 }
