c730fde82f4604524c9526b176982457f6913fd2
Nick Durand <nickfh7@gmail.com>
2023-02-05 23:50:42 -0800

    backup::get_auth_vector clean up and logging

--- services/dauth-service/src/services/backup/get_auth_vector.rs
+++ services/dauth-service/src/services/backup/get_auth_vector.rs
@@ -8,22 +8,23 @@ use crate::data::{
 use crate::database;
 use crate::database::utilities::DauthDataUtilities;
 
-/// Returns the next backup auth vector.
-/// Checks flood vectors first, then auth vector.
-/// Returns auth vector with lowest sequence number.
+/// Gets the next backup auth vector, checking for any available flood
+/// vectors first. If there are no flood vectors, returns the auth vector
+/// with the lowest seqnum.
+#[tracing::instrument(skip(context), name = "backup::get_auth_vector")]
 pub async fn get_auth_vector(
     context: Arc<DauthContext>,
     av_request: &AuthVectorReq,
     signed_request_bytes: &Vec<u8>,
 ) -> Result<AuthVectorRes, DauthError> {
-    tracing::info!("Vector next: {:?}", av_request);
+    tracing::info!("Getting backup auth vector");
 
     let mut transaction = context.local_context.database_pool.begin().await?;
 
     // Check for a flood vector first
     let vector;
-    if let Ok(Some(flood_row)) =
-        database::flood_vectors::get_first(&mut transaction, &av_request.user_id).await
+    if let Some(flood_row) =
+        database::flood_vectors::get_first(&mut transaction, &av_request.user_id).await?
     {
         vector = flood_row.to_auth_vector()?;
 
