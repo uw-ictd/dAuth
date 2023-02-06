bfeff7077e5e81c047bb6341fa275996eb289dab
Nick Durand <nickfh7@gmail.com>
2023-02-06 00:26:34 -0800

    backup::withdraw_shares clean up and logging

--- services/dauth-service/src/services/backup/withdraw_shares.rs
+++ services/dauth-service/src/services/backup/withdraw_shares.rs
@@ -3,13 +3,14 @@ use std::sync::Arc;
 use crate::data::{context::DauthContext, error::DauthError};
 use crate::database;
 
-/// Removes all key shares.
-/// On failure, removes none.
+/// Removes all key shares of the corresponding xres* hash values.
+/// On failure, none of the key shares will be removed.
+#[tracing::instrument(skip(context), name = "backup::withdraw_shares")]
 pub async fn withdraw_shares(
     context: Arc<DauthContext>,
     xres_star_hashs: Vec<&auth_vector::types::XResStarHash>,
 ) -> Result<(), DauthError> {
-    tracing::info!("Handling key shares remove: {:?}", xres_star_hashs,);
+    tracing::info!("Withdrawing key shares");
 
     let mut transaction = context.local_context.database_pool.begin().await?;
     for xres_star_hash in xres_star_hashs {
