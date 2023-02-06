6dd4156e397f05d343fe36871502f2a909391700
Nick Durand <nickfh7@gmail.com>
2023-02-06 00:21:00 -0800

    backup::replace_key_share clean up and logging

--- services/dauth-service/src/services/backup/replace_key_share.rs
+++ services/dauth-service/src/services/backup/replace_key_share.rs
@@ -3,18 +3,15 @@ use std::sync::Arc;
 use crate::data::{context::DauthContext, error::DauthError, keys};
 use crate::database;
 
-/// Replace the old key share if found.
-/// Adds the new key share.
+/// Adds a new key share, replacing the indicated old key share
+/// if it hasn't already been used and removed.
+#[tracing::instrument(skip(context), name = "backup::replace_key_share")]
 pub async fn replace_key_share(
     context: Arc<DauthContext>,
     old_xres_star_hash: &auth_vector::types::XResStarHash,
     new_key_share: &keys::CombinedKeyShare,
 ) -> Result<(), DauthError> {
-    tracing::info!(
-        "Replacing key share: {:?} => {:?}",
-        old_xres_star_hash,
-        new_key_share,
-    );
+    tracing::info!("Replacing key share");
 
     let mut transaction = context.local_context.database_pool.begin().await?;
 
