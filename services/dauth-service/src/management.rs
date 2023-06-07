15c877db949696e32eb98b68d4a00024a13f9690
Nick Durand <nickfh7@gmail.com>
2023-06-07 13:56:12 -0700

    Add withdraw first check to for new users

--- services/dauth-service/src/management.rs
+++ services/dauth-service/src/management.rs
@@ -9,6 +9,21 @@ pub async fn add_user(
     user_info: &UserInfoConfig,
 ) -> Result<(), DauthError> {
     let mut transaction = context.local_context.database_pool.begin().await?;
+    let user_exists = database::user_infos::get(&mut transaction, &user_info.user_id, 0).await.is_ok();
+    transaction.commit().await?;
+
+    // Get all backups that need to have their old vectors removed
+    let existing_backups = if user_exists{
+        transaction = context.local_context.database_pool.begin().await?;
+        let backups = database::backup_networks::get_all(&mut transaction, &user_info.user_id).await?;
+        transaction.commit().await?;
+
+        backups
+    } else {
+        Vec::new()
+    };
+
+    transaction = context.local_context.database_pool.begin().await?;
 
     database::user_infos::upsert(
         &mut transaction,
@@ -25,6 +40,7 @@ pub async fn add_user(
         &user_info.user_id,
         0,
         &context.local_context.id,
+        0,
     )
     .await?;
 
@@ -39,11 +55,18 @@ pub async fn add_user(
         )
         .await?;
 
+        let withdraw = if existing_backups.contains(&backup.backup_id) {
+            1
+        } else{
+            0
+        };
+
         database::tasks::update_users::add(
             &mut transaction,
             &user_info.user_id,
             backup.sqn_slice,
             &backup.backup_id,
+            withdraw,
         )
         .await?;
     }
